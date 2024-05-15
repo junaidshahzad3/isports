1d7c12e40bc8de1d6e6845c19d689ae70131e014
junaidquest <junaid.shahzad@questlab.pk>
2024-05-16 01:09:04 +0500

    images fix

--- frontend/src/components/faculty/FacultyMembers.jsx
+++ frontend/src/components/faculty/FacultyMembers.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import data from "../../constants/facultymembers.json";
+import { facultymembers } from "../../constants/facultymembers";
+
 const FacultyMembers = () => {
-  console.log("data[0]", data[0]);
+  console.log("facultymembers[0]", facultymembers[0]);
   return (
     <div className="flex flex-col text-center items-center my-10">
       <div className="text-3xl font-bold">Faculty Members</div>
@@ -9,18 +10,18 @@ const FacultyMembers = () => {
       <div className="p-6 space-y-2 min-w-[220px] w-full max-w-[300px] hover:shadow-lg rounded-xl border border-transparent hover:border-zinc-200">
         <div className="flex flex-col gap-4 items-center">
           <img
-            src={data[0].image}
+            src={facultymembers[0].image}
             alt=""
             className="w-[150px] h-[150px] shadow-inner object-cover rounded-full border border-zinc-200"
           />
-          <div className="">{data[0].name}</div>
-          <div className="font-bold">{data[0].designation}</div>
+          <div className="">{facultymembers[0].name}</div>
+          <div className="font-bold">{facultymembers[0].designation}</div>
         </div>
       </div>
 
       {/* others */}
       <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 place-items-center">
-        {data?.map((values, index) => {
+        {facultymembers?.map((values, index) => {
           if (index !== 0) {
             return (
               <div
---
+++ frontend/src/constants/facultymembers.js
@@ -0,0 +1,38 @@
+import image1 from "/faculty-image-1.png";
+import image2 from "/faculty-image-2.png";
+import image3 from "/faculty-image-3.png";
+import image4 from "/faculty-image-4.png";
+import image5 from "/faculty-image-5.png";
+
+export const facultymembers = [
+  {
+    id: 1,
+    name: "Qais Asghar",
+    designation: "Editor in Chief",
+    image: image1,
+  },
+  {
+    id: 2,
+    name: "Shahzaib Shahzad",
+    designation: "Sports Correspondents Head",
+    image: image2,
+  },
+  {
+    id: 3,
+    name: "Subhan Akhter",
+    designation: "Director Human Resources",
+    image: image3,
+  },
+  {
+    id: 4,
+    name: "Mr. Sharoz",
+    designation: "Director operations",
+    image: image4,
+  },
+  {
+    id: 4,
+    name: "Sohail Bhatti",
+    designation: "Director Marketing",
+    image: image5,
+  },
+];
